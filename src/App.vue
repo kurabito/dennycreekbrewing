6f6be1b7f2a23df51a0f74fde9a4c3bc22046ee4
Jeff James <jeffjames@gmail.com>
2022-07-02 15:46:22 -0700

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,51 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+
+  <div id="main">
+    <div id="frame">
+      <!-- <dcb:header id="ctlHeader" runat="server"></dcb:header>
+            <div class="spacer">&nbsp;</div>
+            <dcb:nav id="ctlNavTop" runat="server"></dcb:nav> -->
+      <div id="header">
+        <table id="headertable" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td><a href="default.aspx" class="nodecoration"><img src="./assets/icon2.png" border="0"></a></td>
+            <td><a href="default.aspx" class="nodecoration black">Denny Creek Brewing</a></td>
+          </tr>
+        </table>
+      </div>
+      <div id="nav">
+        <div style="padding: 5px;">
+          <a href="/default.aspx">Home</a> |
+          <a href="/calendar.aspx">Calendar</a> |
+          <a href="/logs.aspx">Brew Logs</a> |
+          <a href="/labels.aspx">Labels</a> |
+          <a href="/competitions.aspx">Competitions</a> |
+          <a href="http://dennycreek.blogspot.com" target="_blank">Blog</a> |
+          <a href="http://jbrog.blogspot.com" target="_blank">JBrog</a> |
+          <a href="/resources.aspx">Resources</a> |
+          <a href="/contact.aspx">Contact</a>
+        </div>
+      </div>
+      <div id="content">
+        <Home />
+      </div>
+      <!-- <dcb:footer id="ctlFooter" runat="server"></dcb:footer> -->
+      <!-- To do: set end year dynamically -->
+      <div id="footer">&copy; 2007-2002 Denny Creek Brewing. All rights reserved.</div>
+    </div>
+  </div>
+
+
+
+
 </template>
 
 <style>
@@ -17,5 +57,186 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: #777777;
+  background-color: #CE8600;
+  font-family: Verdana, Arial, Sans-Serif;
+  font-size: smaller;
+  margin: 10px 0px;
+  /* center page content for IE */
+  text-align: center;
+}
+
+a {
+  color: #007F0E;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.nodecoration {
+  font-weight: normal;
+}
+
+.nodecoration:hover {
+  text-decoration: none;
+}
+
+h1 a:hover {
+  text-decoration: none;
+}
+
+td {
+  padding: 10px;
+  font-size: smaller;
+}
+
+img {
+  border: 1px solid black;
+}
+
+#main {
+  width: 800px;
+  font-family: Verdana, Arial, Sans-Serif;
+  /* center page content for Firefox */
+  margin: 0 auto 10px auto;
+  background-color: #777777;
+  background-color: #CE8600;
+}
+
+#frame {
+  /* Force Firefox to show top margin of header
+	border-top: 1px solid black;
+	border-bottom: 1px solid black;
+ */
+}
+
+#header {
+  float: left;
+  text-align: center;
+  vertical-align: bottom;
+  font-family: Papyrus, Verdana, Arial, Sans-Serif;
+  font-weight: bold;
+  font-size: 48px;
+  border: 2px solid black;
+  margin: 6px;
+  margin-bottom: 0;
+  background-color: white;
+  width: 100%;
+}
+
+#header img {
+  border: none;
+}
+
+#headertable {
+  text-align: center;
+  vertical-align: bottom;
+  font-size: 48px;
+}
+
+#nav {
+  float: left;
+  border: 2px solid black;
+  font-size: xx-small;
+  margin: 6px;
+  margin-top: 0;
+  margin-bottom: 0;
+  border-bottom: none;
+  background-color: white;
+  color: Black;
+  width: 100%;
+}
+
+#nav a {}
+
+#content {
+  float: left;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  text-align: left;
+  border: 2px solid black;
+  border-bottom: none;
+  margin: 6px;
+  margin-top: 0;
+  margin-bottom: 0;
+  background-color: white;
+  width: 100%;
+}
+
+#content a {
+  font-weight: bold;
+}
+
+#left {
+  float: left;
+  padding-left: 50px;
+  width: 400px;
+}
+
+#right {
+  float: right;
+  padding-right: 50px;
+}
+
+#single {
+  float: left;
+  width: 100%;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+
+#footer {
+  float: left;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: xx-small;
+  border: 2px solid black;
+  margin: 6px;
+  margin-top: 0;
+  border-top: none;
+  background-color: white;
+  width: 100%;
+}
+
+.spacer {
+  float: left;
+  height: 5px;
+  background-color: #777777;
+  background-color: #CE8600;
+  width: 100%;
+}
+
+.small {
+  font-size: smaller;
+}
+
+.red {
+  color: red;
+}
+
+.black {
+  color: black;
+}
+
+.right {
+  margin: 20px 40px 0 20px;
+  float: right;
+}
+
+.left {
+  margin: 20px 20px 0 0;
+  float: left;
+}
+
+.textbox {
+  width: 200px;
+}
+
+.centered {
+  text-align: center;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+                <div id="left">
+                    <h2>Welcome!</h2>
+                    <p>This is the online home of Denny Creek Brewing. We try to publish all the interesting information
+                        and news about our brewing adventures here.</p>
+                    <p><a href="calendar.aspx">Brewing Calendar</a> - Check out upcoming events at the brewery and other
+                        brewing-related events around town.</p>
+                    <p><a href="logs.aspx">Brew Logs</a> - Brewing notes for each batch we've brewed.</p>
+                    <p><a href="labels.aspx">Labels</a> - Check out some of our bottle labels from past and present
+                        brews.</p>
+                    <p><a href="competitions.aspx">Competitions</a> - We've entered one competition so far. You can see
+                        the judge's comments and final scores in this section of the site.</p>
+                    <p><a href="http://dennycreek.blogspot.com" target="_blank">Blog</a> - The Official Denny Creek
+                        Brewing blog, with random musings from the team (well, OK, only Jeff so far, but I hope the
+                        other guys will jump in soon).</p>
+                    <p><a href="http://jbrog.blogspot.com" target="_blank">JBrog</a> - Jeff's Brewlog, the precursor to
+                        the DCB blog.</p>
+                    <p><a href="resources.aspx">Resources</a> - Lots of great info and advice on brewing.</p>
+                    <p><a href="contact.aspx">Contact Us</a> - Send us your questions and comments. If you're nice and
+                        you include contact info, and we're not swamped, we'll get back to you.</p>
+                </div>
+                <div id="right">
+                    <img src="http://farm1.static.flickr.com/10/12591687_262287e772_m.jpg" /><br />
+                    <br />
+                    <br />
+                    <br />
+                </div>
+                <br />
+                <br />
+                <br />
+            <!-- <dcb:footer id="ctlFooter" runat="server"></dcb:footer> -->
+</template>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
