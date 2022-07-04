9a344ab6a69422a0c3dbf55f795f5349f7b7ede6
Jeff James <jeffjames@gmail.com>
2022-07-04 16:22:49 -0700

    add competitions

--- src/components/Competitions.vue
+++ src/components/Competitions.vue
@@ -2,6 +2,35 @@
 </script>
 
 <template>
+    <div id="single">
+        <h2 align="center">Competitions</h2>
+        <p>We entered an IPA and a wheat beer in the 2007 Evergreen State Fair homebrew competition. The wheat received
+            a white ribbon and the IPA received a red, just one point short of a blue ribbon!</p>
+        <p>Here are some of the documents related to the competition. Click on an image to see a larger version.</p>
+        <table align="center">
+            <tr>
+                <td class="centered"><a href="ESF2007/IPAOverall.bmp" target="_blank" class="small"><img
+                            src="ESF2007/IPAOverall.bmp" width="60" height="85" /><br />IPA<br />Overall</a></td>
+                <td class="centered"><a href="ESF2007/IPA1.jpg" target="_blank" class="small"><img
+                            src="ESF2007/IPA1.jpg" width="60" height="85" /><br />IPA<br />Score #1</a></td>
+                <td class="centered"><a href="ESF2007/IPA2.jpg" target="_blank" class="small"><img
+                            src="ESF2007/IPA2.jpg" width="60" height="85" /><br />IPA<br />Score #2</a></td>
+                <td class="centered"><a href="ESF2007/WheatOverall.bmp" target="_blank" class="small"><img
+                            src="ESF2007/WheatOverall.bmp" width="60" height="85" /><br />Wheat<br />Overall</a></td>
+                <td class="centered"><a href="ESF2007/Wheat1.jpg" target="_blank" class="small"><img
+                            src="ESF2007/Wheat1.jpg" width="60" height="85" /><br />Wheat<br />Score #1</a></td>
+                <td class="centered"><a href="ESF2007/Wheat2.jpg" target="_blank" class="small"><img
+                            src="ESF2007/Wheat2.jpg" width="60" height="85" /><br />Wheat<br />Score #2</a></td>
+                <td class="centered"><a href="ESF2007/award.jpg" target="_blank" class="small"><img
+                            src="ESF2007/award.jpg" width="60" height="85" /><br />Award<br />Check</a></td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    </div>
 </template>
 
 <style scoped>
