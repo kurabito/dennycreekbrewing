0a08e1eb83474bde8c9121d369b9f42a15d9b22b
Jeff James <jeffjames@gmail.com>
2022-07-04 15:12:53 -0700

    add brew logs

--- src/components/BrewLogs.vue
+++ src/components/BrewLogs.vue
@@ -5,71 +5,56 @@
     <div id="left">
         <h2>Brew Logs</h2>
         <p>Here are detailed notes from our brewing sessions.</p>
-        <a href="1-AmberAle.txt" target="_blank">#1 - Amber Ale</a>
-
-
-        <!-- <table>
+        <table>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_2cs3gn2&hl=en"
-                        target="_blank">#1 - Amber Ale</a></td>
+                <td><a href="1-AmberAle.txt" target="_blank">#1 - Amber Ale</a></td>
                 <td>2/3/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_3dwtspk&hl=en"
-                        target="_blank">#2 - Hopitat IPA</a></td>
+                <td><a href="2-HopitatIPA.txt" target="_blank">#2 - Hopitat IPA</a></td>
                 <td>3/25/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_59f7kxtf5&hl=en"
-                        target="_blank">#3 - IPA</a></a></td>
+                <td><a href="3-IPA.txt" target="_blank">#3 - IPA</a></td>
                 <td>5/26/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_6f9dbk8hp&hl=en"
-                        target="_blank">#4 - Weihenstephan Wheat</a></a></td>
+                <td><a href="4and5-Wheat.txt" target="_blank">#4 - Weihenstephan Wheat</a></td>
                 <td>6/16/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_6f9dbk8hp&hl=en"
-                        target="_blank">#5 - American Wheat</a></a></td>
+                <td><a href="4and5-Wheat.txt" target="_blank">#5 - American Wheat</a></td>
                 <td>6/16/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_7ggcgf3cc&hl=en"
-                        target="_blank">#6 - Festivus Ale</a></a></td>
+                <td><a href="6-Festivus.txt" target="_blank">#6 - Festivus Ale</a></td>
                 <td>9/1/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?docid=dc3p264r_82xfrnhdv&hl=en"
-                        target="_blank">#7 - Fresh-Hopped IPA</a></a></td>
+                <td><a href="7-IPA.txt" target="_blank">#7 - Fresh-Hopped IPA</a></td>
                 <td>9/16/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?id=dc3p264r_9ntzg5fck"
-                        target="_blank">#8 - Scottish Ale</a></a></td>
+                <td><a href="8-Scottish" target="_blank">#8 - Scottish Ale</a></td>
                 <td>11/4/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?id=dc3p264r_10cdg3rsgw"
-                        target="_blank">#9 - Amber Ale</a></a></td>
+                <td><a href="9-Amber.txt" target="_blank">#9 - Amber Ale</a></td>
                 <td>12/16/07</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?id=dc3p264r_11cpwpjng8"
-                        target="_blank">#10 - Festivus Ale</a></a></td>
+                <td><a href="10-Festivus.txt" target="_blank">#10 - Festivus Ale</a></a></td>
                 <td>1/19/08</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?id=dc3p264r_12dg2j84fg"
-                        target="_blank">#11 - Hopitat IPA</a></a></td>
+                <td><a href="11-Hopitat.txt" target="_blank">#11 - Hopitat IPA</a></td>
                 <td>2/10/08</td>
             </tr>
             <tr>
-                <td><a href="http://docs.google.com/a/dennycreekbrewing.com/Doc?id=dc3p264r_13cjd2bffh"
-                        target="_blank">#12 - Amarillo Pale Ale</a></a></td>
+                <td><a href="12-Amarillo.txt" target="_blank">#12 - Amarillo Pale Ale</a></td>
                 <td>3/1/08</td>
             </tr>
-        </table> -->
+        </table>
     </div>
     <div id="right">
         <img src="http://farm1.static.flickr.com/92/251539122_c91719c7c2_m.jpg" /><br />
@@ -77,9 +62,6 @@
         <br />
         <br />
     </div>
-
-
-
 </template>
 
 <style scoped>
